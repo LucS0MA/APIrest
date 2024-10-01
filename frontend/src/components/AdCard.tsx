2755ab03e8de16e7baa2424ba93ee43441a1ae33
Lucien KOHLI <lucien.kohli@gmail.com>
2024-10-01 16:01:30 +0200

    css

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -15,11 +15,12 @@ function AdCard({ title, picture, price, link, category }: AdCardProps) {
   return (
     <div className="ad-card-container">
       <a className="ad-card-link" href={link}>
-        <div className="ad-card-category">{category.title}</div>
+        <div className="ad-card-text">
+          <div className="ad-card-category">{category.title}</div>
+        </div>
         <img className="ad-card-image" src={picture} />
         <div className="ad-card-text">
           <div className="ad-card-title">{title}</div>
-
           <div className="ad-card-price">{price}</div>
         </div>
       </a>
